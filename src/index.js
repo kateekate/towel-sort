c374ab7c433688d2669a97064a9ddcf7c0f32a66
Katya <katya.mikhaylova.98@list.ru>
2022-11-06 01:34:35 +0300

    the solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+module.exports = function towelSort(matrix) {
+  if (!matrix) { return [] }
+  let matrixTowelSort = []
+  matrix.forEach((el, i) => {
+    if (i % 2 != 0) matrixTowelSort.concat(el.reverse())
+    matrixTowelSort = matrixTowelSort.concat(el)
+  })
 
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
+  return matrixTowelSort
 }
